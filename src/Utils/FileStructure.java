40b9e351836ad16fb7bd42c3e3990d4f1474ba28
Luatan <simonschmid@hispeed.ch>
2021-09-13 20:33:55 +0200

    cleanup import

--- src/Utils/FileStructure.java
+++ src/Utils/FileStructure.java
@@ -4,7 +4,6 @@ import org.apache.commons.io.FileUtils;
 import java.io.IOException;
 import java.io.File;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileStructure {

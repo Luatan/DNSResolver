ad58cb2fa2285fd5fff5b7e2221fdfdb6b19b62f
Luatan <simonschmid@hispeed.ch>
2021-09-13 22:42:40 +0200

    added create dir function to copy the whole dir from ressources

--- src/Utils/FileStructure.java
+++ src/Utils/FileStructure.java
@@ -3,6 +3,7 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.IOException;
 import java.io.File;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -28,6 +29,47 @@ public class FileStructure {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static void createDir(String dirPath) {
+        if (!FileStructure.directoryExists(dirPath)){
+            try {
+                Files.createDirectories(Paths.get(FileStructure.DIR_HOME + dirPath));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void createFile(String ressourcePath, String destPath) {
+        if (FileStructure.fileExists(destPath)){
+            return;
+        }
+
+        // try to get Stream
+        try (InputStream is = FileStructure.class.getClassLoader().getResourceAsStream(ressourcePath)) {
+            // convert input stream to file
+            FileUtils.copyInputStreamToFile(is, new File(FileStructure.DIR_HOME + destPath));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void copyDirFromRessources(String srcPath, String destPath) {
+        File test;
+        try {
+            test = new File(FileStructure.class.getClassLoader().getResource(srcPath).getFile());
+        } catch (NullPointerException e){
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            FileUtils.copyDirectoryToDirectory(test, new File(FileStructure.DIR_HOME + destPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 

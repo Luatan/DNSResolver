704f45d11247a282a9ade5b3d5d2afdb7bc6972c
Luatan <simonschmid@hispeed.ch>
2021-12-01 00:16:18 +0100

    settings file has now a template

--- src/Utils/Config.java
+++ src/Utils/Config.java
@@ -8,10 +8,10 @@ import java.util.Objects;
 public class Config {
     public static boolean CACHING = true;
     public static int CACHE_TIME_TO_LIVE = 20;
+    public static final String CACHE_FILES = "cache/";
     public static final String WHOIS_CONF_FILE = "config/whois_servers.json";
     public static final String HISTORY_CONF_FILE = "logs/history.json";
     public static final String SETTINGS_CONF_FILE = "config/settings.json";
-    public static final String CACHE_FILES = "cache/";
 
     public static void createWhoisConfig() {
         if (!FileStructure.fileExists(WHOIS_CONF_FILE)){
@@ -21,20 +21,14 @@ public class Config {
 
     public static void createHistoryConfig(){
         if (!FileStructure.fileExists(HISTORY_CONF_FILE)) {
-            JSONObject obj = new JSONObject();
-            obj.put("domains", new String[]{});
+            JSONObject obj = new JSONObject().put("domains", new String[]{});
             FileStructure.createFile(obj.toString(4), HISTORY_CONF_FILE);
         }
     }
 
     public static void createSettingsConfig() {
-        if (!FileStructure.fileExists(SETTINGS_CONF_FILE)) {
-            JSONObject jsonObj = new JSONObject();
-            jsonObj.put("darkmode", false);
-            jsonObj.put("ShowEmptyRecords", false);
-            jsonObj.put("cache", true);
-            jsonObj.put("cacheTime", 20);
-            FileStructure.createFile(jsonObj.toString(4), SETTINGS_CONF_FILE);
+        if (!FileStructure.fileExists(SETTINGS_CONF_FILE)){
+            FileStructure.createFileFromPath("config/default_settings.json", SETTINGS_CONF_FILE);
         }
     }
 

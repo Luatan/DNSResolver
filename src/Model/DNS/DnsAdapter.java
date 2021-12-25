4935e0678fcf11cde3418a01204be325a101f358
Simon <simonschmid@hispeed.ch>
2021-12-25 21:46:33 +0100

    added missing srv Record

--- src/Model/DNS/DnsAdapter.java
+++ src/Model/DNS/DnsAdapter.java
@@ -25,7 +25,6 @@ public class DnsAdapter {
         if (!type.equals("NS")){
             setRecords(type);
         }
-        System.out.println(records);
     }
 
     private void setHost(String host) {
